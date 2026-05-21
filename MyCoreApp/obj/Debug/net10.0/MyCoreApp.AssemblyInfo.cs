1802525bd95ed1589ddc4c43d46c7c2f1b2b7428
Eduardo Machado <eduardomachado05080@gmail.com>
2026-05-21 11:35:08 +0000

    c

--- MyCoreApp/obj/Debug/net10.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/obj/Debug/net10.0/MyCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af82d55f42028f0d6b5b93d362f0665405467ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bbf511fe49f4afcb30762d43ce6fb46fe78d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
